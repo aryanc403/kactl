9ded142072cd54a4f092298d1b8405ad5ef403a8
Abhay Pratap Singh <abhaypratapsingh258@gmail.com>
2019-12-24 16:27:11 +0530

    Add faulhaber number

---
+++ content/number-theory/Faulhaber.cpp
@@ -0,0 +1,26 @@
+/*Description: It computes sum_{i=1}^{i=n}i^p in O(p).
+  It assumes C[n][k], inv[n] and pow(a, x)(a^x % mod) are defined.
+ */
+void pre(){
+  B[0] = 1;
+  B[1] = -(mod+1)/2; //Bernoulli Nos
+  for(int i = 2;i<51;i+=2){
+    rep(k,i){
+      B[i]+=(C[i+1][k]*B[k])%mod;
+    }
+    B[i]%=mod;
+    B[i] = ((-B[i]*inv[i+1])%mod+mod)%
+      mod;
+  }
+}
+ll Faul(ll n,int p){
+  ll ans = 0;
+  repA(i,1,p+1){
+    ll cur = B[p+1-i]*modpow(n,i)%mod;
+    cur = cur*C[p+1][i]%mod;
+    if(i==p) ans-=cur;
+    else ans+=cur;
+  }
+  return ((ans%mod+mod)%mod)*modpow(p+1,
+      mod-2)%mod;
+}

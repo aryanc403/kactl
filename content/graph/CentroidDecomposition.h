6e6d8f7c3377ecac072b69485633ae06c3d94680
Abhay Pratap Singh <abhaypratapsingh258@gmail.com>
2019-12-24 23:51:38 +0530

    Update centroid decomposition(remove one log)

--- content/graph/CentroidDecomposition.h
+++ content/graph/CentroidDecomposition.h
@@ -2,15 +2,16 @@
   It finds the centroid tree for given tree.
   par[x] denotes centroid parent of x.
   Initialize global variables correctly before using.
-*/
-set<int> adj[maxN+1];
+  */
+vector<int> adj[maxN+1];
 vi sz, par;
-int nn = 0;
+vector<bool> block;
+int nn;
 void dfs0(int x, int p) {
   nn++;
   sz[x] = 1;
   for(auto v: adj[x]) {
-    if(v!=p) {
+    if(v!=p && !block[v]) {
       dfs0(v,x);
       sz[x] += sz[v];
     }
@@ -18,7 +19,7 @@ void dfs0(int x, int p) {
 }
 int dfs1(int x, int p) {
   for(auto v: adj[x]) {
-    if(v!=p && sz[v]>nn/2)
+    if(v!=p && !block[v] && sz[v]>nn/2)
       return dfs1(v, x);
   }
   return x;
@@ -26,14 +27,13 @@ int dfs1(int x, int p) {
 // Parent of root is parent only
 void decompose(int root, int p) {
   nn=0;
-  dfs0(root, root); // Taking parent in dfs call as root won't affect dfs :p
+  dfs0(root, root); 
   int centroid = dfs1(root, root);
   if(p==-1) p = centroid;
   par[centroid] = p;
+  block[centroid] = true;
   for(auto v: adj[centroid]) {
-    adj[v].erase(centroid);
-    decompose(v, centroid);
+    if(!block[v])
+      decompose(v, centroid);
   }
-  adj[centroid].clear(); 
 }
-
